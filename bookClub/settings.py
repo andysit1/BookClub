31346bc1501d0d53eac99deca808bd3845d94ebf
Demon Lord <84219113+the-demonKid@users.noreply.github.com>
2022-12-28 13:45:46 -0500

    Update settings.py

--- bookClub/settings.py
+++ bookClub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ebri4qn!o-ko4=8(=6$znbit2c80+$5d8%@sc_&!j-t2(tjp0'
+SECRET_KEY = 'key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
